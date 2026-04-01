53bb09c93cb9a35dbe3905884cf3f53e535aca7c
IvanKhudyakov <halex7@yandex.ru>
2026-04-01 17:32:52 +0300

    Add browser addoption

--- 1.py
+++ 1.py
@@ -8,14 +8,14 @@ import math
 class TestLoginUserAndCorrectAnswer():
     
     @pytest.mark.parametrize('link', [
-        #"https://stepik.org/lesson/236895/step/1",
+                                      "https://stepik.org/lesson/236895/step/1",
                                       "https://stepik.org/lesson/236896/step/1",
                                       "https://stepik.org/lesson/236897/step/1",
-                                    #  "https://stepik.org/lesson/236898/step/1",
-                                     # "https://stepik.org/lesson/236899/step/1",
-                                      #"https://stepik.org/lesson/236903/step/1",
-                                      #"https://stepik.org/lesson/236904/step/1",
-                                    #  "https://stepik.org/lesson/236905/step/1"
+                                      "https://stepik.org/lesson/236898/step/1",
+                                      "https://stepik.org/lesson/236899/step/1",
+                                      "https://stepik.org/lesson/236903/step/1",
+                                      "https://stepik.org/lesson/236904/step/1",
+                                      "https://stepik.org/lesson/236905/step/1"
                                     ]
                               )
     def test_login_and_correct_answer(self, browser, load_config, link):
--- conftest.py
+++ conftest.py
@@ -5,10 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(10)
     yield browser
     print("\nfinish browser for test..")
